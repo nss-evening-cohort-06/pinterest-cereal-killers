6da704e669d4f685b956e82d832e476c6a8261a6
Brooke Lewis <belv2c@gmail.com>
2017-11-28 19:35:29 -0600

    added pin getter function

--- javascripts/services/PinService.js
+++ javascripts/services/PinService.js
@@ -1,1 +1,27 @@
 "use strict";
+
+app.service("PinService", function($http, $q, FIREBASE_CONFIG) {
+
+	const getAllPins = (userUid) => {
+		let pinArray = [];
+		return $q ((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/pko.json?orderBy="pinId"&equalTo="${userUid}"`).then((results) => {
+				let pins = results.data;
+				Object.keys(pins).forEach((key) => {
+					pins[key].id = key;
+					pinArray.push(pins[key]);
+					});
+					
+					resolve(pinArray);			
+					console.log(pinArray);	
+					
+			}).catch((err) => {
+				console.log("error in getAllThePins", err);
+			});
+		});
+	};
+
+
+return {getAllPins};
+
+});
