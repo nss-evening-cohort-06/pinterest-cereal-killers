f2ebc65f6cd1a3d2f9da44c8e840d45a52bc710d
Brooke Lewis <belv2c@gmail.com>
2017-12-02 10:44:30 -0600

    pin object info prints in save/edit form

--- javascripts/controllers/ViewEditCtrl.js
+++ javascripts/controllers/ViewEditCtrl.js
@@ -4,21 +4,36 @@ app.controller("ViewEditCtrl", function($location, $rootScope, $routeParams, $sc
 
 
 	const getPins = () => {
-		PinService.getSinglePin($routeParams.id).then((results) => {
+		PinService.getSinglePin($routeParams.id).then((results) => { 
 			$scope.pins = results.data;
 		}).catch((err) => {
 			console.log("error in getPins", err);
 		});
 	};
 
+$scope.newpin = {};
 
-	$scope.savePin = (newPin) => {
-		PinService.updatePin(newPin, $routeParams.id).then((results) => {
-			$location.path("/contacts/view");
+
+	const garbageFunction = () => { 
+		let newpin = {};
+		
+		$scope.newpin.board = $rootScope.updatedPin.board;
+		$scope.newpin.title = $rootScope.updatedPin.title;
+		$scope.newpin.url = $rootScope.updatedPin.url;
+		$scope.newpin.notes = $rootScope.updatedPin.notes;
+		$scope.newpin.datePinned = $rootScope.updatedPin.datePinned;
+
+	};
+		
+/*		PinService.updatePin(newpin, $routeParams.id).then((results) => {
+			$location.path("/pinterest/boards");
 		}).catch((err) => {
 			console.log("error in submitForm", err);
 		});
-	};
-
+	
 	getPins();
+};
+*/
+garbageFunction();
+
 });
