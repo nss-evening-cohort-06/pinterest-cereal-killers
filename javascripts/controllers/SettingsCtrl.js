13bdf1d954e32361b1149bae5f3091afd53b1d9f
Juno Lee <lejuno80@gmail.com>
2017-12-02 10:04:38 -0600

    settings

--- javascripts/controllers/SettingsCtrl.js
+++ javascripts/controllers/SettingsCtrl.js
@@ -1,5 +1,37 @@
 "use strict";
 
-app.controller("SettingsCtrl", function($scope) {
-	$scope.controller = "SettingsCtrl";
+app.controller("SettingsCtrl", function($scope, $location, $rootScope, SettingsSevice){
+	$scope.newProfileSaved = {};
+
+	// $scope.getNewAvatar = ()=> {
+		//**Gina
+	// };
+
+	$scope.saveProfileButton = ()=> {
+		let newProfilesaved = {
+			"uid": $rootScope.uid,
+			"firstName": $scope.newProfileSaved.firstName,
+			"lastName": $scope.newProfiledSaved.lastName,
+			"bio": $scope.newProfileSaved.bio
+		};
+		//Juno did not create SettingService yet
+		 SettingsService.addUpdatedProfile(newProfileSaved).then(() => {
+		 $location.path('/settings');
+		    }).catch((err) => {
+			console.log("err in addNewContact", err);
+		    });
+	
+	};
+	
+
+
+	$scope.cancelProfile = () => {
+		$location.path('/view');
+	};
+
+
+
+
+
+
 });
